6163b748ef9dbad0b4b373646a62b642db5b0863
ItzCandra23 <95993220+ItzCandra23@users.noreply.github.com>
2022-09-27 11:14:56 +0700

    Update 2.1.0

--- README.md
+++ README.md
@@ -1,18 +1,23 @@
-# Economy-X 2.0.1
-Added language and fixed bugs.
+# Economy-X 2.1.0
+This plugin added economy system.
 
 # Commands
-* /mymoney - Check your money.
-* /money <target: Player> - Check player money.
-* /transfer <target: Player> <amount: number> - Transfer money target.
-* /addmoney <target: Player> <amount: number> - Add player money.
-* /removemoney <target: Player> <amount: number> - Remove player money.
-* /setmoney <target: Player> <amount: number> - Set player money.
+* /mymoney - `Check your money.`
+* /money <target: ServerPlayer> - `Check player money.`
+* /transfer <target: ServerPlayer> <amount: number> - `Transfer money target.`
+* /addmoney <target: ServerPlayer> <amount: number> - `Add player money.`
+* /removemoney <target: ServerPlayer> <amount: number> - `Remove player money.`
+* /setmoney <target: ServerPlayer> <amount: number> - `Set player money.`
 
 # Permissions
-* /mymoney - ```Normal```
-* /money - ```Normal```
-* /transfer - ```Normal```
-* /addmoney - ```Operator```
-* /removemoney - ```Operator```
-* /setmoney - ```Operator```
+* /mymoney - `Normal`
+* /money - `Normal`
+* /transfer - `Normal`
+* /addmoney - `Operator`
+* /removemoney - `Operator`
+* /setmoney - `Operator`
+
+# Updates 2.1.0
+* Fix language bug.
+* Added EconomyWeb class.
+* Added XCoin.
--- src/api/index.ts
+++ src/api/index.ts
@@ -2,14 +2,14 @@ import { ServerPlayer } from "bdsx/bds/player";
 import { events } from "bdsx/event";
 import { Configuration } from "../util/configuration";
 import { Language } from "../util/lang";
-
-const fs = require("fs");
+import * as fs from "fs";
+import { TextPacket } from "bdsx/bds/packets";
 
 /**Economy Response */
 export enum ecoR {
-    NotFound = -1,
-    Invalid,
-    Success,
+    NotFound = "NotFound",
+    Invalid = "Invalid",
+    Success = "Success",
 }
 
 interface player_data {
@@ -22,24 +22,29 @@ let db: {
     player_data
 } = {};
 
-let config = {
+export let config = {
     "currency": "$"
 };
 
-try { config = require( __dirname + "../../config.json") } catch(e) { console.log(`[Economy-X] config.json Error!`) }
-try { db = require( __dirname + "../../player_data.json") } catch(e) { console.log(`[Economy-X] player_data.json Error!`) }
+try { config = require( __dirname + "../../../data/config.json") } catch(e) { console.log(`[Economy-X] config.json Error!`) }
+try { db = require( __dirname + "../../../data/player_data.json") } catch(e) { console.log(`[Economy-X] player_data.json Error!`) }
 
 
 const configuration: Configuration = new Configuration("EN_us");
 const language: Language = new Language(configuration.language);
 
-/**Translate a texts. */
-export function translate(texts: string): string {
+function trle(texts: string): string {
     const text = language.translate(texts);
 
     if (text === undefined) return texts;
 
     return text;
+}
+
+/**Translate a texts. */
+export function translate(texts: string): string {
+    const text = trle(texts);
+    return text.substring(0, text.length-1);
 }
 
 /**Economy-X. */
@@ -114,7 +119,7 @@ export class EconomyX {
 
     /**Get player money. */
     static getMoney(player: ServerPlayer): number {
-        if (player.getXuid() === undefined) return ecoR.NotFound;
+        if (player.getXuid() === undefined) return 0;
         if (db.hasOwnProperty(player.getXuid()) === false) this.addPlayer(player);
 
         const data = db[player.getXuid()];
@@ -144,6 +149,28 @@ export class EconomyX {
         data2.money+amount;
         return ecoR.Success;
     }
+
+    /**WriteConfigFile. */
+    static writeConfig(): void {
+        fs.writeFile(__dirname + "../../../data/config.json", JSON.stringify(config), (err) => {
+            if (err) {
+                console.log(`[Economy-X] config.json Error! \n${err}`);
+            } else {
+                console.log(`[Economy-X] config.json Success!`);
+            }
+        });
+    }
+
+    /**WritePlayerDataFile. */
+    static writeData(): void {
+        fs.writeFile(__dirname + "../../../data/player_data.json", JSON.stringify(db), (err) => {
+            if (err) {
+                console.log(`[Economy-X] player_data.json Error! \n${err}`);
+            } else {
+                console.log(`[Economy-X] player_data.json Success!`);
+            }
+        });
+    }
 }
 
 events.playerJoin.on((ev) => {
@@ -151,6 +178,182 @@ events.playerJoin.on((ev) => {
 });
 
 events.serverStop.on(() => {
-    fs.writeFile(__dirname + "../../config.json", JSON.stringify(config), () => {});
-    fs.writeFile(__dirname + "../../player_data.json", JSON.stringify(db), () => {});
+    EconomyX.writeConfig();
+    EconomyX.writeData();
 });
+
+
+/**NEW EconomyWeb. */
+export class EconomyWeb {
+    /**Check player data. */
+    static check(xuid: string): boolean {
+        return db.hasOwnProperty(xuid);
+    }
+
+    /**Get player data. */
+    static getPlayer(xuid: string): player_data|null {
+        if (db.hasOwnProperty(xuid) === false) return null;
+        return db[xuid];
+    }
+
+    /**Add player money. */
+    static addMoney(xuid: string, amount: number): ecoR {
+        if (db.hasOwnProperty(xuid) === false) return ecoR.NotFound;
+
+        const data = db[xuid];
+
+        if (amount < 1) return ecoR.Invalid;
+
+        data.money=data.money+amount;
+        return ecoR.Success;
+    }
+
+    /**Remove player money. */
+    static removeMoney(xuid:string, amount: number): ecoR {
+        if (db.hasOwnProperty(xuid) === false) return ecoR.NotFound;
+
+        const data = db[xuid];
+
+        if (amount < 1) return ecoR.Invalid;
+        if (data.money-amount < 0) {
+            data.money=0;
+            return ecoR.Success;
+        }
+
+        data.money=data.money-amount;
+        return ecoR.Success;
+    }
+
+    /**Set player money. */
+    static setMoney(xuid: string, amount: number): ecoR {
+        if (db.hasOwnProperty(xuid) === false) return ecoR.NotFound;
+
+        const data = db[xuid];
+
+        if (amount < 0) return ecoR.Invalid;
+
+        data.money=amount;
+        return ecoR.Success;
+    }
+
+    /**Get player money. */
+    static getMoney(xuid: string): number|null {
+        if (db.hasOwnProperty(xuid) === false) return null;
+
+        const data = db[xuid];
+
+        return data.money;
+    }
+}
+
+export class XCoin {
+    /**Get player X-Coin. */
+    static get(player: ServerPlayer): number {
+        if (player.getXuid() === undefined) return 0;
+        if (db.hasOwnProperty(player.getXuid()) === false) EconomyX.addPlayer(player);
+
+        const data = db[player.getXuid()];
+        return data.xcoin;
+    }
+
+    /**Add player X-Coin. */
+    static add(player: ServerPlayer, amount: number): ecoR {
+        if (player.getXuid() === undefined) return ecoR.NotFound;
+        if (db.hasOwnProperty(player.getXuid()) === false) EconomyX.addPlayer(player);
+
+        const data = db[player.getXuid()];
+
+        if (amount < 1) return ecoR.Invalid;
+
+        data.xcoin=data.xcoin+amount;
+        return ecoR.Success;
+    }
+
+    /**Remove player X-Coin. */
+    static remove(player: ServerPlayer, amount: number): ecoR {
+        if (player.getXuid() === undefined) return ecoR.NotFound;
+        if (db.hasOwnProperty(player.getXuid()) === false) EconomyX.addPlayer(player);
+
+        const data = db[player.getXuid()];
+
+        if (amount < 1) return ecoR.Invalid;
+        if (data.xcoin-amount < 0) {
+            data.xcoin=0;
+            return ecoR.Success;
+        }
+
+        data.xcoin=data.xcoin-amount;
+        return ecoR.Success;
+    }
+
+    /**Set player X-Coin. */
+    static set(player: ServerPlayer, amount: number): ecoR {
+        if (player.getXuid() === undefined) return ecoR.NotFound;
+        if (db.hasOwnProperty(player.getXuid()) === false) EconomyX.addPlayer(player);
+
+        const data = db[player.getXuid()];
+
+        if (amount < 0) return ecoR.Invalid;
+
+        data.xcoin=amount;
+        return ecoR.Success;
+    }
+}
+
+export class XCoinWeb {
+    /**Get player X-Coin. */
+    static get(xuid: string): number {
+        if (db.hasOwnProperty(xuid) === false) return 0;
+
+        const data = db[xuid];
+        return data.xcoin;
+    }
+
+    /**Add player X-Coin. */
+    static add(xuid: string, amount: number): ecoR {
+        if (db.hasOwnProperty(xuid) === false) return ecoR.NotFound;
+
+        const data = db[xuid];
+
+        if (amount < 1) return ecoR.Invalid;
+
+        data.xcoin=data.xcoin+amount;
+        return ecoR.Success;
+    }
+
+    /**Remove player X-Coin. */
+    static remove(xuid: string, amount: number): ecoR {
+        if (db.hasOwnProperty(xuid) === false) return ecoR.NotFound;
+
+        const data = db[xuid];
+
+        if (amount < 1) return ecoR.Invalid;
+        if (data.xcoin-amount < 0) {
+            data.xcoin=0;
+            return ecoR.Success;
+        }
+
+        data.xcoin=data.xcoin-amount;
+        return ecoR.Success;
+    }
+
+    /**Set player X-Coin. */
+    static set(xuid: string, amount: number): ecoR {
+        if (db.hasOwnProperty(xuid) === false) return ecoR.NotFound;
+
+        const data = db[xuid];
+
+        if (amount < 0) return ecoR.Invalid;
+
+        data.xcoin=amount;
+        return ecoR.Success;
+    }
+}
+
+export function sendTransslate(player: ServerPlayer, message: string): void {
+    const pk = TextPacket.allocate();
+    pk.type = TextPacket.Types.Raw;
+    pk.message = translate(message);
+    player.sendNetworkPacket(pk);
+    pk.dispose();
+}

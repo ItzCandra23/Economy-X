104f6895779d6cbc13dbdb7e0c03a74ded25bd08
ItzCandra23 <95993220+ItzCandra23@users.noreply.github.com>
2023-02-01 00:20:25 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./src");
-const event_1 = require("bdsx/event");
-event_1.events.serverOpen.on(() => {
-    console.log(`[Economy-X] Started!`);
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLGlCQUFlO0FBQ2Ysc0NBQW9DO0FBRXBDLGNBQU0sQ0FBQyxVQUFVLENBQUMsRUFBRSxDQUFDLEdBQUcsRUFBRTtJQUN0QixPQUFPLENBQUMsR0FBRyxDQUFDLHNCQUFzQixDQUFDLENBQUM7QUFDeEMsQ0FBQyxDQUFDLENBQUMifQ==
